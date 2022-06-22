6936233e7faa1ef89c95986665c614a00a0de4a2
unknown <kalsijatin67@gmail.com>
2022-06-21 23:59:15 -0400

    home page

--- src/components/albums-list.js
+++ src/components/albums-list.js
@@ -1,20 +1,71 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Component} from 'react'
+import { Link } from 'react-router-dom' 
 import axios from 'axios'
 
-export default function AlbumsList() {
-    const [albums, setAlbums] = useState([])
+const Album = props => {
+    <tr>
+        <td>{props.album.title}</td>
+        <td>{props.album.artist}</td>
+        <td>{props.album.description}</td>
+        <td>{props.album.duration}</td>
+        <td>{props.album.date.substring(0, 10)}</td>
+        <td>
+            <Link to={`/edit/${props.album._id}`}>edit</Link> | <a href='#' onClick={() => {props.deleteAlbum(props.album._id)}}>delete</a>
+        </td>
+    </tr>
+}
 
-    useEffect(() => {
+export default class AlbumsList extends Component {
+    constructor(props) {
+        super(props)
+
+        this.deleteAlbum = this.deleteAlbum.bind(this)
+        this.state = {
+            albums: []
+        }
+    }
+
+    albumsList() {
+        return this.state.albums.map(album => {
+            return <Album album={album} deleteAlbum={this.deleteAlbum} key={album._id}/>
+        })
+    }
+
+    deleteAlbum(id) {
+        axios.delete(`http://localhost:5000/albums/${id}`)
+        .then(res => console.log(res.data))
+        this.setState({
+            albums: this.state.albums.filter(album => album._id !== id)
+        })
+    }
+
+    componentDidMount() {
         axios.get('http://localhost:5000/albums')
         .then(res => {
-            if (res.data) setAlbums(res.data)
+            this.setState({albums: res.data})
         })
-    })
+        .catch(err => console.log(err))
+    }
 
-
-    return (
-        <div>
-            <p>you are on the albums list component</p>
-        </div>
-    )
+    render() {
+        return (
+            <div>
+                <h3>Logged Albums</h3>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Artist</th>
+                            <th>Description</th>
+                            <th>Duration</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.albumsList()}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 }
