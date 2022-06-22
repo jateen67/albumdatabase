03e799e17b8e4926809d62d2c29c402173f85400
unknown <kalsijatin67@gmail.com>
2022-06-21 22:44:18 -0400

    styling almost done

--- src/components/add-album.js
+++ src/components/add-album.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState, Component, useEffect } from 'react'
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 export default function AddAlbum() {
+    //all the state based on hte album model
+    const [title, setTitle] = useState('')
+    const [artist, setArtist] = useState('')
+    const [description, setDescription] = useState('')
+    const [duration, setDuration] = useState(0)
+    const [date, setDate] = useState(new Date())
+    //state purely for this component
+    const [artists, setArtists] = useState([])
+
+    const changeTitle = (e) => {
+        setTitle(e.target.value)
+    }
+    const changeArtist = (e) => {
+        setArtist(e.target.value)
+    }
+    const changeDescription = (e) => {
+        setDescription(e.target.value)
+    }
+    const changeDuration = (e) => {
+        setDuration(e.target.value)
+    }
+    const changeDate = (e) => {
+        setDate(e)
+    }
+    const addAlbumClicked = (e) => {
+        e.preventDefault()
+        const album = {title, artist, description, duration, date}
+        console.log(album)
+        window.location = '/'
+    }
+    useEffect(() => {
+        setArtist('test artist')
+        setArtists(['test artist'])
+    }, [])
+
     return (
         <div>
-            <p>you are on the add album component</p>
+            <h3>Add New Album</h3>
+            <form onSubmit={addAlbumClicked}>
+                <div className='form-group'>
+                    <label>Title: </label>
+                    <input className='form-control' value={title} onChange={changeTitle} required></input>
+                </div>
+                <div className='form-group'>
+                    <label>Artist: </label>
+                    <select className='form-control' required value={artist} onChange={changeArtist}>
+                        {
+                            artists.map((artist) => {
+                                return <option key={artist} value={artist}>{artist}</option>
+                            })
+                        }
+                    </select>
+                </div>
+                <div className='form-group'>
+                    <label>Description: </label>
+                    <input className='form-control' value={description} onChange={changeDescription} required></input>
+                </div>
+                <div className='form-group'>
+                    <label>Duration: </label>
+                    <input className='form-control' value={duration} onChange={changeDuration} required></input>
+                </div>
+                <div className='form-group'>
+                    <label>Date Released: </label>
+                    <DatePicker className='form-control' selected={date} onChange={changeDate} required/>
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-primary' type='submit' required>Add Album</button>
+                </div>
+            </form>
         </div>
     )
 }
--- src/components/add-artist.js
+++ src/components/add-artist.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AddArtist() {
+        //all the state based on hte album model
+        const [artist, setArtist] = useState('')
+        //state purely for this component
+        const [artists, setArtists] = useState([])
+    
+        const changeArtist = (e) => {
+            setArtist(e.target.value)
+        }
+        const addArtistClicked = (e) => {
+            e.preventDefault()
+            const artistAdded = {artist}
+            console.log(artistAdded)
+            setArtist('')
+        }
+
     return (
         <div>
-            <p>you are on the add artist component</p>
+            <h3>Add New Artist</h3>
+            <form onSubmit={addArtistClicked}>
+                <div className='form-group'>
+                    <label>Artist Name: </label>
+                    <input className='form-control' value={artist} onChange={changeArtist} required></input>
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-primary' type='submit'>Add Artist</button>
+                </div>
+            </form>
         </div>
     )
 }
